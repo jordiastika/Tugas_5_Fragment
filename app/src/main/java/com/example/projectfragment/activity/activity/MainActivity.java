48cc3d9817295e65ad4d4694a5bb5cf582457268
jordiastika <74273993+jordiastika@users.noreply.github.com>
2021-04-24 13:57:36 +0800

    Initial commit

---
+++ app/src/main/java/com/example/projectfragment/activity/activity/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.projectfragment.activity.activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.projectfragment.R;
+import com.example.projectfragment.activity.fragment.FavoriteFragment;
+import com.example.projectfragment.activity.fragment.HomeFragment;
+import com.example.projectfragment.activity.fragment.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView buttomNav = findViewById(R.id.buttom_navigation);
+        buttomNav.setOnNavigationItemSelectedListener(navListener);
+
+        //memunculkan fragment di awal
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container,new HomeFragment())
+                .commit();
+
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_favorite:
+                            selectedFragment = new FavoriteFragment();
+                            break;
+                        case R.id.nav_search:
+                            selectedFragment = new SearchFragment();
+                            break;
+                    }
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment_container, selectedFragment)
+                            .commit();
+                    return true;
+                }
+        };
+}
